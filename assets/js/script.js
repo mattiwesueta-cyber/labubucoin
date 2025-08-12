88082f9d3eca75c32e8446db4f8b50627243a92d
mattiewsueta-cyber <matthew.norton@tmail.link>
2025-08-12 10:31:10 +0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1702,7 +1702,7 @@ ${referralUrl}`;
             numbersCont.appendChild(span);
             setTimeout(() => {
                 span.style.opacity = '0';
-                span.style.transform += ' scale(1) translateY(-10px)';
+                span.style.transform += ' scale(1) translateY(-20px)';
             }, 150);
             setTimeout(() => {
                 numbersCont.removeChild(span);
